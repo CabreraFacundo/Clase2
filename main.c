3d77d74fbc83c0ac0deebd4ec21fa711404c988b
alumno <alumno@lab6pc0>
2018-08-23 21:02:43 -0300

    primer comentario

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+    int numeroMax;
+    int numeroMin;
+    int i;
+    float promedio;
+    int acumulador;
+    acumulador = 0;
+
+    for(i=0;i<10;i++)
+    {
+        printf("Ingrese un numero: ");
+        scanf("%d",&numero);
+        acumulador = acumulador + numero;
+
+        if(i==0)
+        {
+            numeroMax=numero;
+            numeroMin=numero;
+        }
+        if(numero < numeroMin)
+        {
+            numeroMin=numero;
+        }
+        if(numero > numeroMax)
+        {
+            numeroMax=numero;
+        }
+    }
+
+    promedio = (float)acumulador / i;
+    printf("El numero maximo es: %d",numeroMax);
+    printf("\nEl numero minimo es: %d",numeroMin);
+    printf("\nEl promedio es: %f",promedio);
+
+    return 0;
+}
